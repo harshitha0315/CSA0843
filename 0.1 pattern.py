58cae99821a83e09ec3eb1dd0dd17dfcf5656876
harshitha0315/192111390 <112242455+harshitha0315@users.noreply.github.com>
2022-09-29 13:09:44 +0530

    Add files via upload

---
+++ 0.1 pattern.py
@@ -0,0 +1,15 @@
+try:
+	r=int(input("Enter number of rows="))
+	if(r<=0):
+		print("Invalid input")
+		exit()
+	else:
+		for i in range(1,r+1):
+			k=0.1
+			for j in range(1,i+1):
+				print("%.1f"%k,end=" ")
+			print("\n")
+except ValueError:
+			print("Invalid")
+			
+	
